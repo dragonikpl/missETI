6a5220bd2ea1528732981a95af3c50c901a8d6c4
Lukasz Domaradzki <dragonikpl@gmail.com>
2012-08-27 21:52:27 +0200

    inversive generator - not tested

--- missETI/src/misseti/generators/InversiveGenerator.java
+++ missETI/src/misseti/generators/InversiveGenerator.java
@@ -1,13 +1,64 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package misseti.generators;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
- * @author imac
+ * @author ldomaradzki
  */
 public class InversiveGenerator {
     
+    private int a, c;
+    private int m;
+    private List X;
+    
+    public InversiveGenerator() {
+        X = new ArrayList<Integer>();
+    }
+    
+    public void Initialize(int a, int c, int m, int x0) {
+        this.a = a;
+        this.c = c;
+        this.m = m;
+        
+        X.add(x0);
+    }
+    
+    public int Next() {
+        int next, previous;
+        previous = (Integer) X.get(X.size() - 1);
+        
+        if (previous != 0) {
+            next = (a * modInverse(previous, m) + c) % m;
+        } else {
+            next = c;
+        }
+        
+        
+        X.add(next);
+        
+        return next;
+    }
+
+    int modInverse(int a, int n) {
+        
+        int i = n, v = 0, d = 1;
+        while (a > 0) 
+        {
+            int t = i / a, x = a;
+            a = i % x;
+            i = x;
+            x = d;
+            d = v - t * x;
+        }     
+        
+        v %= n;      
+        if (v < 0) {
+            v = (v + n) % n;
+        }
+        return v;
+        
+    }
 }
