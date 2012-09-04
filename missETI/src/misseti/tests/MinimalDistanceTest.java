088456d38509f8a82b2428903dec9dcf7cf74336
Lukasz Domaradzki <dragonikpl@gmail.com>
2012-09-04 19:53:53 +0200

    uprzatniecie

--- missETI/src/misseti/tests/MinimalDistanceTest.java
+++ missETI/src/misseti/tests/MinimalDistanceTest.java
@@ -12,29 +12,28 @@ import misseti.generators.GenericGenerator;
  *
  * @author akolassa
  */
-public class RandomSpheresTest 
+public class MinimalDistanceTest 
 {
-            
-    public class Point3D
+    //deklaracja zmiennych        
+    public class Point2D
     {
         public double x;
         public double y;
-        public double z;
-        public double radius;
+        public double distance;
     }
-    
-    public static final double MAXRADIUS = Double.MAX_VALUE;
-    public static final int loops = 20;
+
+    public static final double MAXDISTANCE = Double.MAX_VALUE;
+    public static final int loops = 100;
     public long edge;
     public long pointNumber;
     public GenericGenerator generator;
-    public ArrayList<Point3D> points;
+    public ArrayList<Point2D> points;
     
-    public RandomSpheresTest()
+    public MinimalDistanceTest()
     {
     }
     
-    public RandomSpheresTest(GenericGenerator generator, long edge, long pointNumber)
+    public MinimalDistanceTest(GenericGenerator generator, long edge, long pointNumber)
     {
         this.generator = generator;
         this.edge = edge;
@@ -44,53 +43,50 @@ public class RandomSpheresTest
     public boolean runTest()
     {
         double[] pValues = new double[loops];
-        points = new ArrayList<Point3D>();
-        double minRadius, minRadius3;
+        points = new ArrayList<Point2D>();
+        double minDistance, minDistance2;
         
         
         for (int loop = 0; loop < loops; loop++)
         {
             points.clear();
-            minRadius = MAXRADIUS;
-            minRadius3 = 0.0;
+            minDistance = MAXDISTANCE;
+            minDistance2 = 0.0;
 
             for (int i = 0; i < pointNumber; i++)
             {
-                Point3D p = new Point3D();
+                Point2D p = new Point2D();
                             
                 p.x = Double.valueOf(generator.getRandom() % edge);
                 p.y = Double.valueOf(generator.getRandom() % edge);
-                p.z = Double.valueOf(generator.getRandom() % edge);
-                p.radius = MAXRADIUS;
+                p.distance = MAXDISTANCE;
 
-                for (Point3D neighbour : points)
+                for (Point2D neighbour : points)
                 { 
                     double dX = neighbour.x - p.x;
                     double dY = neighbour.y - p.y;
-                    double dZ = neighbour.z - p.z;
                     
-                    double r2 = dX * dX + dY * dY + dZ * dZ;
+                    double r2 = dX * dX + dY * dY;
                     double r1 = Math.sqrt(r2);
-                    double r3 = r2 * r1;
                     
-                    if (r1 < minRadius)
+                    if (r1 < minDistance)
                     {
-                        minRadius = r1;
-                        minRadius3 = r3;
+                        minDistance = r1;
+                        minDistance2 = r2;
                     }
                 }
                 
-                //System.out.println(p.radius);
+                //System.out.println(p.distance);
                 
                 points.add(i, p);
             }
 
-            double p = 1.0 - Math.exp(-1.0 * minRadius3 / 30.0);
+            double p = 1.0 - Math.exp(-1.0 * minDistance2 / 0.995);
             
             pValues[loop] = p;
 
-            System.out.println("MINRADIUS: " + minRadius);
-            System.out.println("MINRADIUS3: " + minRadius3);
+            System.out.println("MINDISTANCE: " + minDistance);
+            System.out.println("MINDISTANCE2: " + minDistance2);
             System.out.println("[LOOP " + (loop + 1) + "] P = " + p);
         }
         
